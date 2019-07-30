31e81ef0058a5392354df6ea84c02a2edd46abbc
Gavin Ballard <gavin@gavinballard.com>
2019-07-30 19:41:14 +1000

    Add CJS module prepublish step

--- index.js
+++
@@ -1,4 +0,0 @@
-import { Submarine } from './src/submarine';
-import { SubmarinePaymentMethodStep } from './src/checkout/submarine_payment_method_step';
-
-export { Submarine, SubmarinePaymentMethodStep };
