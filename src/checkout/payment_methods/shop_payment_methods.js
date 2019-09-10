8e6e754b453f56ec904c0a9f71adeceb50f6deef
Gavin Ballard <gavin@gavinballard.com>
2019-09-10 14:50:28 +1000

    Add Cybersource credit card support

--- src/checkout/payment_methods/shop_payment_methods.js
+++ src/checkout/payment_methods/shop_payment_methods.js
@@ -1,10 +1,14 @@
 import { ShopPaymentMethod } from "./shop_payment_methods/shop_payment_method";
 import { BraintreeCreditCardShopPaymentMethod } from "./shop_payment_methods/braintree_credit_card_shop_payment_method";
+import { CybersourceCreditCardShopPaymentMethod } from "./shop_payment_methods/cybersource_credit_card_shop_payment_method";
 import { StripeCreditCardShopPaymentMethod } from "./shop_payment_methods/stripe_credit_card_shop_payment_method";
 
 const SHOP_PAYMENT_METHODS = {
   'braintree': {
     'credit-card': BraintreeCreditCardShopPaymentMethod
+  },
+  'cybersource': {
+    'credit-card': CybersourceCreditCardShopPaymentMethod
   },
   'stripe': {
     'credit-card': StripeCreditCardShopPaymentMethod
