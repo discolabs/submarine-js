5bbaab7552d5b74cf4b4ba992a2cd1432da6a5ba
Gavin Ballard <gavin@gavinballard.com>
2022-02-05 21:23:28 +1100

    temp

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import * as path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    lib: {
-      entry: path.resolve(__dirname, 'lib/submarine.js'),
-      name: 'Submarine',
-      fileName: (format) => `submarine.${format}.js`
-    },
+    //lib: {
+    //entry: path.resolve(__dirname, 'lib/submarine.js'),
+    //  name: 'submarine-js',
+    //  fileName: (format) => `submarine.${format}.js`
+    //},
     rollupOptions: {
-      external: ['react']
+      input: {
+        submarine: resolve(__dirname, 'lib/submarine.js')
+      },
+      output: [
+        {
+          format: 'es',
+          entryFileNames: 'submarine.es.js',
+          dir: resolve(__dirname, 'dist')
+        },
+        {
+          format: 'iife',
+          entryFileNames: 'submarine.js',
+          dir: resolve(__dirname, 'dist'),
+          name: 'Submarine'
+        }
+      ]
     }
   },
-  plugins: [react()]
-})
+  plugins: [
+    react()
+    //legacy({
+    //  targets: ['ie >= 11'],
+    //  additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+    //})
+  ]
+});
